827a5c03a8eefae0060b2afd3c0edd78a80b577c
Trần Duy Quang <tranduyquang2205@gmail.com>
2024-11-06 03:31:43 +0700

    update

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ class IPWhitelistMiddleware:
     async def __call__(self, request: Request, call_next):
         client_ip = request.client.host
         if client_ip not in self.allowed_ips:
-            return APIResponse.json_format({'result': False, 'message': 'Unknown error!'})
+            return APIResponse.json_format({'code':503,'result': False, 'message': 'Unknown error!'})
         return await call_next(request)
 # Map the class names to the actual classes
 BANK_CLASSES = {
