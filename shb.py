2a95179cdfcffbb5a9b24215058437c28a86d46d
Trần Duy Quang <tranduyquang2205@gmail.com>
2024-11-21 19:44:20 +0700

    update

--- shb.py
+++ shb.py
@@ -30,8 +30,8 @@ class SHB:
             self.proxy_info = random.choice(self.proxy_list)
             proxy_host, proxy_port, username_proxy, password_proxy = self.proxy_info.split(':')
             self.proxies = {
-                'http': f'socks5://{username_proxy}:{password_proxy}@{proxy_host}:{proxy_port}',
-                'https': f'socks5://{username_proxy}:{password_proxy}@{proxy_host}:{proxy_port}'
+                'http': f'http://{username_proxy}:{password_proxy}@{proxy_host}:{proxy_port}',
+                'https': f'http://{username_proxy}:{password_proxy}@{proxy_host}:{proxy_port}'
             }
         else:
             self.proxies = None
